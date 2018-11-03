efab93f2ee4df2aa0fcf2b03a1e6ee6c02561330
Jacob <jds110@live.missouristate.edu>
2018-11-03 16:51:23 -0500

    Fixed sizing

--- index.php
+++ index.php
@@ -29,23 +29,6 @@
 			"augue euismod sodales.");
 		}
 	</script>
-	<div class = "mainWrapper">
-		<div class = "talkTextButtons">
-			<button class = "talkTextButton talkButton"><i class="fas fa-phone"></i></button>
-			<button class = "talkTextButton textButton"><i class="fas fa-comments"></i></button>
-
-		</div>
-
-		<DIV class = "mainButtons">
-			<div class = "mainButton login">
-				<a href="/user/login">Login</a>
-			</div>
-			<div class = "mainButton create">
-				<a href="/user/new">Create Account</a>
-			</div>
-		</DIV>
-	</div>
-
 	<script type="text/javascript" src = "js/main.js"></script>
 
 <?php include 'includes/footer.html';?>
