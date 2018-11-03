23b2aad1053722a5be1ef315d5a851449b0eb006
Jett Walley <ben.walley@classyllama.com>
2018-11-03 13:33:27 -0500

    fix conflicts

--- index.php
+++ index.php
@@ -12,22 +12,6 @@
 
 	
 
-	
-	<div class = "mainWrapper">
-		<div class = "talkTextButtons">
-			<button class = "talkTextButton talkButton"><i class="fas fa-phone"></i></button>
-			<button class = "talkTextButton textButton"><i class="fas fa-comments"></i></button>
-
-		</div>
-
-		<div class = "mainButtons">
-			<div class = "mainButton login">
-				<a href="/user/login">Login</a>
-			</div>
-			<div class = "mainButton create">
-				<a href="/user/new">Create Account</a>
-			</div>
-		</div>
 
      
 
