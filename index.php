083fb42742dc45496d9a8830e47539eae0a17979
Jett Walley <ben.walley@classyllama.com>
2018-11-03 13:08:13 -0500

    more stling

--- index.php
+++ index.php
@@ -16,13 +16,14 @@
 		<div class = "talkTextButtons">
 			<button class = "talkTextButton talkButton"><i class="fas fa-phone"></i></button>
 			<button class = "talkTextButton textButton"><i class="fas fa-comments"></i></button>
-
 		</div>
-		<div class = "mainButton login">
-			<a href="/user/login">Login</a>
-		</div>
-		<div class = "mainButton create">
-			<a href="/user/new">Create Account</a>
+		<div class = "mainButtons">
+			<div class = "mainButton login">
+				<a href="/user/login">Login</a>
+			</div>
+			<div class = "mainButton create">
+				<a href="/user/new">Create Account</a>
+			</div>
 		</div>
 	</div>
 		
