9c06f1dacfd1d6155d531e93f82cedaa86605e9e
Jett Walley <ben.walley@classyllama.com>
2018-11-03 00:39:01 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+</head>
+<body>
+	<div>
+		<a href="/user/login">Login</a>
+	</div>
+
+	<script type="text/javascript" src = "js/main.js"></script>
+</body>
+</html>
